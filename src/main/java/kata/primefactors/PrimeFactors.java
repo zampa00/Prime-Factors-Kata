07ac6ca59c68af5843332d1350c68c5c48d9ccaa
zampa <zampa92@gmail.com>
2020-07-03 11:07:09 +0200

    testFifteen green

--- src/main/java/kata/primefactors/PrimeFactors.java
+++ src/main/java/kata/primefactors/PrimeFactors.java
@@ -7,9 +7,13 @@ public class PrimeFactors {
     public static ArrayList<Integer> generate(int i) {
         ArrayList<Integer> primeFactors = new ArrayList<Integer>();
 
-        while (i % 2 == 0) {
-            primeFactors.add(2);
-            i /= 2;
+        int primeCandidate = 2;
+        while (i > 1) {
+            while (i % primeCandidate == 0) {
+                primeFactors.add(primeCandidate);
+                i /= primeCandidate;
+            }
+            primeCandidate++;
         }
         if (i > 1) {
             primeFactors.add(i);
