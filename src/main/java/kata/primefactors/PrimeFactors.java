c9fce3a9ca360415a2d0bd2d3ba65d9b86f50c2a
zampa <zampa92@gmail.com>
2020-07-03 11:12:23 +0200

    refactor

--- src/main/java/kata/primefactors/PrimeFactors.java
+++ src/main/java/kata/primefactors/PrimeFactors.java
@@ -7,13 +7,11 @@ public class PrimeFactors {
     public static ArrayList<Integer> generate(int i) {
         ArrayList<Integer> primeFactors = new ArrayList<Integer>();
 
-        int primeCandidate = 2;
-        while (i > 1) {
+        for (int primeCandidate = 2; primeCandidate <= i; primeCandidate++) {
             while (i % primeCandidate == 0) {
                 primeFactors.add(primeCandidate);
                 i /= primeCandidate;
             }
-            primeCandidate++;
         }
 
         return primeFactors;
