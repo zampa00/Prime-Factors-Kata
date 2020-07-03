c9bc63c289a00858aea0d828af26a21bec6b2286
zampa <zampa92@gmail.com>
2020-07-03 10:12:06 +0200

    testOne green

--- src/main/java/kata/primefactors/PrimeFactors.java
+++ src/main/java/kata/primefactors/PrimeFactors.java
@@ -1,4 +1,13 @@
 package kata.primefactors;
 
+import java.util.ArrayList;
+
 public class PrimeFactors {
+
+    public static ArrayList<Integer> generate(int i) {
+        ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+
+        return primeFactors;
+    }
+
 }
